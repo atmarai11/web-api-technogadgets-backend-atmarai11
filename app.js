9f742fe9425f9658436fa467f363ae4c13b63ef5
Shiva Adhikari <shivaadhikari499@gmail.com>
2022-06-25 02:12:02 +0545

    Correcting the port number

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 
+app.use(express.json());
+
 require("./database/databaseConnnection");
+
+app.use(require("./routes/customerRoute"));
+
+app.listen(90, () => {
+  console.log("Listening at port 90.");
+});
