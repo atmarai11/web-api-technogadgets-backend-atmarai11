ef4d6142ea1493979fb6c958b9a0c9053fda43aa
Shiva Adhikari <shivaadhikari499@gmail.com>
2022-06-27 00:10:31 +0545

    add route to get reviews by user

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 const { customerGuard } = require("../auth/auth");
 
-const { addReview, getBookReviews } = require("../controller/reviewController");
+const {
+  addReview,
+  getBookReviews,
+  getUserReviews,
+} = require("../controller/reviewController");
 
 router.route("/review/add/:productId").post(customerGuard, addReview);
 
 router.route("/reviews/get/book/:productId").get(customerGuard, getBookReviews);
+
+router.route("/reviews/get/user").get(customerGuard, getUserReviews);
 
 module.exports = router;
