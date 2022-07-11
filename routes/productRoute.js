24aa5976236fb3ce2c04f98a72ede71945c21cb8
Shiva Adhikari <shivaadhikari499@gmail.com>
2022-07-11 16:45:06 +0545

    add routes for gettting product by id and for getting all products from database and import corresponding controller functionality to the file

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -7,9 +7,13 @@ const {
   getProduct,
   updateProduct,
   deleteProduct,
+  getAllProudcts,
+  getProductById,
 } = require("../controller/productController");
 const imgUpload = require("../upload/imageUpload");
 
+router.get("/book/get/:id", getProductById);
+router.get("/book/get/all", getAllProudcts);
 router.get("/book/get/category=:category", getProduct);
 router.post("/book/add", customerGuard, imgUpload.single("img"), addProduct);
 router.put("/book/update/:productId", customerGuard, updateProduct);
