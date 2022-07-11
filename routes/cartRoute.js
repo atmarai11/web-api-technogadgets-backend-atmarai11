32961cc4eb3dcbdc54ab11ebd82af5f6c4f078ff
Shiva Adhikari <shivaadhikari499@gmail.com>
2022-07-11 16:40:01 +0545

    add route for updating the cart

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const { customerGuard } = require("../auth/auth");
-const { addCart, getCart } = require("../controller/cartController");
+const {
+  addCart,
+  getCart,
+  updateCart,
+} = require("../controller/cartController");
 
 router.route("/cart/add/:productId").post(customerGuard, addCart);
 router.route("/cart/getAll").get(customerGuard, getCart);
+router.route("/cart/update/:cartId").put(customerGuard, updateCart);
 
 module.exports = router;
