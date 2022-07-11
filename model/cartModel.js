89b20452dc18da5bf47d77828ff60b0ca06ce25b
Shiva Adhikari <shivaadhikari499@gmail.com>
2022-07-11 15:26:28 +0545

    complete addition of cart model

---
+++ model/cartModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Cart = mongoose.Schema({
+  productId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Cart", Cart);
