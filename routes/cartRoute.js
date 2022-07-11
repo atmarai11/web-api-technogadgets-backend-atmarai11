169c312bab8abbe563542c5bf6e38457718bc3ef
Shiva Adhikari <shivaadhikari499@gmail.com>
2022-07-11 16:40:19 +0545

    add route for deleting the cart

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -6,10 +6,12 @@ const {
   addCart,
   getCart,
   updateCart,
+  deleteCart,
 } = require("../controller/cartController");
 
 router.route("/cart/add/:productId").post(customerGuard, addCart);
 router.route("/cart/getAll").get(customerGuard, getCart);
 router.route("/cart/update/:cartId").put(customerGuard, updateCart);
+router.route("/cart/delete/:cartId").delete(customerGuard, deleteCart);
 
 module.exports = router;
